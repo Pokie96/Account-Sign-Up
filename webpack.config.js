84f26bd324d7345217a1f76376e7ae538ab8e8d2
adam <19adamchristie96@gmail.com>
2024-01-09 22:20:24 +0000

    Install and use the babel loader in our webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,19 @@ const config = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }   
+                }
+            },
         ],
     },
 };
